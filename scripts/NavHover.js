9ab358554dd8f39e2a291a9c06f1702c4a8b08c3
Br1gery <pipecppc7@gmail.com>
2023-11-24 16:05:17 +0500

    nav animations code optimisation

--- scripts/NavHover.js
+++ scripts/NavHover.js
@@ -4,47 +4,23 @@ let supp = document.querySelector('.nav__supp')
 let about = document.querySelector('.nav__about')
 let login = document.querySelector('.nav__login')
 
-let main_anim = gsap.to('.line__main',{
-    paused: true,
-    width: '100%',
-    duration: 0.4,
-})
+let main_anim
+let shop_anim
+let supp_anim
+let about_anim
+let login_anim
 
-main.addEventListener('mouseover', ()=>main_anim.play())
-main.addEventListener('mouseout', ()=>main_anim.reverse())
+let anims = [main_anim, shop_anim, supp_anim, about_anim, login_anim]
+let a = [main, shop, supp, about, login]
+let lines = ['.line__main', '.line__shop', '.line__supp', '.line__about', '.line__login']
 
-let shop_anim = gsap.to('.line__shop',{
-    paused: true,
-    width: '100%',
-    duration: 0.4,
-})
+for (let i = 0; i < anims.length; i++) {
+    anims[i] = gsap.to(lines[i], {
+        paused: true,
+        width: '100%',
+        duration: 0.4,
+    })
 
-shop.addEventListener('mouseover', ()=>shop_anim.play())
-shop.addEventListener('mouseout', ()=>shop_anim.reverse())
-
-let supp_anim = gsap.to('.line__supp',{
-    paused: true,
-    width: '100%',
-    duration: 0.4,
-})
-
-supp.addEventListener('mouseover', ()=>supp_anim.play())
-supp.addEventListener('mouseout', ()=>supp_anim.reverse())
-
-let about_anim = gsap.to('.line__about',{
-    paused: true,
-    width: '100%',
-    duration: 0.4,
-})
-
-about.addEventListener('mouseover', ()=>about_anim.play())
-about.addEventListener('mouseout', ()=>about_anim.reverse())
-
-let login_anim = gsap.to('.line__login',{
-    paused: true,
-    width: '100%',
-    duration: 0.4,
-})
-
-login.addEventListener('mouseover', ()=>login_anim.play())
-login.addEventListener('mouseout', ()=>login_anim.reverse())
+    a[i].addEventListener('mouseover', () => anims[i].play())
+    a[i].addEventListener('mouseout', () => anims[i].reverse())
+}
